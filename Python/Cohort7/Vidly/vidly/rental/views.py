e6185ce1804ce71ed7e3eb4eacfab13bb17515e1
Marlo <marlo.baguyos@outlook.com>
2020-05-14 19:57:14 -0400

    update

--- Python/Cohort7/Vidly/vidly/rental/views.py
+++ Python/Cohort7/Vidly/vidly/rental/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
-
-
 # Create your views here.
 # MVC python manage.py runserver
 
 # Defining an endpoint
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, 'index.html')
 
 # about send your name
 

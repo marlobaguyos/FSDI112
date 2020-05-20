3d885e24fcb0e8ec8b54fe3812da3db04d4f1857
Marlo <marlo.baguyos@outlook.com>
2020-05-20 15:55:54 -0400

    update

--- Python/Cohort7/Vidly/vidly/api/models.py
+++ Python/Cohort7/Vidly/vidly/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL
 from rental.models import Movie, Genre
+from tastypie.authorization import Authorization
 
 
 # Create your models here.
@@ -8,8 +9,34 @@ class MovieResource(ModelResource):
     class Meta:
         queryset = Movie.objects.all()
         resource_name = 'movies'
+        ordering = ['title', 'release_year', 'price']
+        filtering = {
+            'release_year': ALL, 
+            'price': ALL,
+            'title': ALL
+        }
+        allowed_method = ['get', 'post', 'patch', 'put', 'delete', 'options']
+        authorization = Authorization()
 
 class GenreResource(ModelResource):
     class Meta:
         queryset = Genre.objects.all()
         resource_name = 'genres'
+        allowed_method = ['get', 'post', 'patch', 'put', 'delete', 'options']
+        authorization = Authorization()
+
+
+"""
+
+Ordering:
+/movies/?order_by=price
+/movies/?order_by=-price
+
+Filtering:
+/movies/?price__lt=10
+/movies/?price__gt=10
+/movies/?price=10
+/movies/?release_year__gt=2018
+/movies/?title__contains=forest
+
+"""

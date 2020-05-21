f31b96bf1f3d15adb56d301a10ff43503c56c4b9
Marlo <marlo.baguyos@outlook.com>
2020-05-20 21:10:39 -0400

    update

--- Python/Cohort7/Vidly/vidly/rental/static/scripts/catalog.js
+++ Python/Cohort7/Vidly/vidly/rental/static/scripts/catalog.js
@@ -1,8 +1,22 @@
 
+var catalog = [];
 
+function loadData() {
+    $.ajax({
+        url: '/api/movies',
+        type: 'GET',
+        success: (res) => {
+            console.log('From server', res);
+        },
+        error: (errorDetials) => {
+            console.log(errorDetails);
+        }
+    });
+}
 
 function init(){
     console.log("Catalog page");
+    loadData();
 }
 
 window.onload = init;
